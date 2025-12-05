187b1f48a179c17e5161fc904ab3a3661c399cd3
Pergl <per.glansa@gmail.com>
2025-12-05 10:23:05 +0100

    tagit bort paket helt i calculator.java och calculatoTest.java

--- src/main/java/CiDemo1Application.java
+++ src/main/java/CiDemo1Application.java
@@ -1,4 +1,4 @@
-package systementor.cidemo1;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/CiDemo1ApplicationTests.java
+++ src/test/java/CiDemo1ApplicationTests.java
@@ -1,4 +1,4 @@
-package systementor.cidemo1;
+
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
